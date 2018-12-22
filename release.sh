19266b7382c2b3680b8c583d2ef27718dbeaf662
zengxuebing <zengxb94@foxmail.com>
2018-12-22 20:40:36 +0800

    第一次发布完成

--- release.sh
+++ release.sh
@@ -14,4 +14,4 @@ cd ../
 scp AcFun-CIP-OS.zip me_tencent:~/
 
 ssh me_tencent 'rm -rf ~/AcFun-CIP-OS && unzip AcFun-CIP-OS.zip -d AcFun-CIP-OS 1>/dev/nul'
-# ssh me_tencent 'export PATH=$PATH:/root/.nvm/versions/node/v10.14.2/bin/node && pm2 stop AcFun-CIP-OS && cd AcFun-CIP-OS && knex migrate:latest && pm2 start AcFun-CIP-OS'
+ssh me_tencent 'export PATH=$PATH:/root/.nvm/versions/node/v10.14.2/bin && pm2 stop AcFun-CIP-Official-Site && cd AcFun-CIP-OS && knex migrate:latest && pm2 start AcFun-CIP-Official-Site'
